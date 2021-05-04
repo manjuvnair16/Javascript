6eb274ea449e0fcfc970e24c287696a1f07d9076
manjuvnair16 <75314631+manjuvnair16@users.noreply.github.com>
2021-05-04 09:57:11 +0100

    Create shortestWordCount.js

---
+++ CodeWars/level 7/shortestWordCount.js
@@ -0,0 +1,21 @@
+//Given a string of words, find the count of the shortest word
+
+function findShort(s){
+  let wordArr = s.split(' ').map(word => word.length);
+  let shortestWord = Math.min(...wordArr);
+  return shortestWord;
+  
+}
+
+console.log(findShort("bitcoin take over the world maybe who knows perhaps"));
+
+/*console log
+3
+*/
+
+-------------------------------------
+/*Alternate solutions
+
+function findShort(s){
+  return Math.min.apply(null, s.split(' ').map(w => w.length));
+}
